a1917ddf8660d622b03c807782b344e9dd3c62b4
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-07-28 00:59:16 +0700

    hql

--- src/main/java/com/devtiro/database/repositories/AuthorRepository.java
+++ src/main/java/com/devtiro/database/repositories/AuthorRepository.java
@@ -1,5 +1,6 @@
 package com.devtiro.database.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,8 @@ import com.devtiro.database.domain.Author;
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
     Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a from Author a where a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
 
 }
