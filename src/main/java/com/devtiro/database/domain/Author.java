e864ed5f598dbe3b22a7714899626a76dcc72559
RAfi123-cmd <rafiulhuda27@gmail.com>
2024-07-27 14:27:28 +0700

    create entities

--- src/main/java/com/devtiro/database/domain/Author.java
+++ src/main/java/com/devtiro/database/domain/Author.java
@@ -10,8 +10,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "authors")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
     private Long id;
 
     private String name;
--- src/main/java/com/devtiro/database/domain/Book.java
+++ src/main/java/com/devtiro/database/domain/Book.java
@@ -1,5 +1,11 @@
 package com.devtiro.database.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +15,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
     private String isbn;
 
     private String title;
 
+    // jika kita mendapatkan sebuah buku kembali kami juga mengambil penulis jika
+    // kami harus mengambil penulis
+    // aktif buku ini membuat beberapa perubahan pada itu perubahan kemudian akan
+    // diubah ke database.
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "author_id")
     private Author author;
 
 }
